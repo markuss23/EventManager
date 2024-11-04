a160712c21d57b923cfb5745121441efcc00755c
marek.tremel <tremelmarek@gmail.com>
2024-11-04 22:09:03 +0100

    redis yuhu

--- api/app/src/events/routes.py
+++ api/app/src/events/routes.py
@@ -1,27 +1,35 @@
+import json
 from bson import ObjectId
 from fastapi import APIRouter, HTTPException, Request
+from redis import Redis
 
 from app.src.events.schemas import EventCreate, Event, EventOwner
-from app.databases import mongo_client
-
+from app.databases import mongo_client, redis_client
 
 router = APIRouter(prefix="/events", tags=["events"])
 
 mongo_client = mongo_client.get_db()
 collection = mongo_client["events"]
 
+redis_client: Redis | None = redis_client.get_db()
+
 
 @router.get("/", response_model=list[Event])
 def get_events(request: Request) -> list[Event]:
-    events = collection.find()
-    return list(events)
+    cache_key = "events:list_all"
+
+    cached_events = redis_client.get(cache_key)
+    if cached_events:
+        events = [Event(**event) for event in json.loads(cached_events)]
+        return events
+
+    events = list(collection.find())
+    redis_client.setex(cache_key, 300, json.dumps(events, default=str))
+    return events
 
 
 @router.post("/", response_model=Event)
-def create_event(
-    request: Request,
-    event: EventCreate,
-) -> Event:
+def create_event(event: EventCreate) -> Event:
     new_event = {
         "_id": str(ObjectId()),
         "title": event.title,
@@ -32,48 +40,78 @@ def create_event(
         "attendees": event.attendees,
     }
     collection.insert_one(new_event)
+
+    # Invalidate the cache for all events and the specific user's events
+    redis_client.delete("events:list_all")
+    redis_client.delete(f"user_events:{event.owner_id}")
+
     return new_event
 
 
 @router.get("/{event_id}", response_model=Event)
-def get_event(event_id: str, request: Request) -> Event:
-    event = collection.find_one({"_id": event_id})
-    if event:
-        return event
-    raise HTTPException(status_code=404, detail="Event not found")
+def get_event(event_id: str) -> Event:
+    cache_key = "events:list_all"
+
+    cached_events = redis_client.get(cache_key)
+    if cached_events:
+        events = json.loads(cached_events)
+        for event in events:
+            if event["_id"] == event_id:
+                return Event(**event)
+
+    events = list(collection.find())
+    redis_client.setex(cache_key, 300, json.dumps(events, default=str))
+
+    event = next((event for event in events if event["_id"] == event_id), None)
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+
+    return Event(**event)
 
 
 @router.put("/{event_id}", response_model=Event)
-def update_event(event_id: str, event: EventCreate, request: Request) -> Event:
+def update_event(event_id: str, event: EventCreate) -> Event:
     existing_event = collection.find_one({"_id": event_id})
-    if existing_event:
-        collection.update_one({"_id": event_id}, {"$set": dict(event)})
-        return event
-    raise HTTPException(status_code=404, detail="Event not found")
+    if not existing_event:
+        raise HTTPException(status_code=404, detail="Event not found")
+
+    collection.update_one({"_id": event_id}, {"$set": dict(event)})
+
+    # Invalidate the cache for all events and the specific user's events
+    redis_client.delete("events:list_all")
+    redis_client.delete(f"user_events:{event.owner_id}")
+
+    updated_event = {**dict(event), "_id": event_id}
+    return updated_event
 
 
 @router.get("/user/{user_id}", response_model=list[EventOwner])
-def get_user_events(user_id: str, request: Request) -> list[EventOwner]:
+def get_user_events(user_id: str) -> list[EventOwner]:
+    cache_key = f"user_events:{user_id}"
+
+    cached_user_events = redis_client.get(cache_key)
+    if cached_user_events:
+        print("Cache hit for user events")
+        events = json.loads(cached_user_events)
+        return [EventOwner(**event) for event in events]
+
     pipeline = [
+        {"$match": {"owner_id": user_id}},  # Match events for the specific user
         {
             "$lookup": {
-                "from": "users",  # Kolekce "users"
-                "localField": "owner_id",  # Pole z kolekce "events"
-                "foreignField": "_id",  # Pole z kolekce "users"
-                "as": "owner",  # Výstupní pole
+                "from": "users",
+                "localField": "owner_id",
+                "foreignField": "_id",
+                "as": "owner",
             }
         },
-        {
-            "$unwind": "$owner"  # Rozbalí pole "owner" pro jednotlivé události
-        },
+        {"$unwind": "$owner"},
     ]
 
     events = list(collection.aggregate(pipeline))
+    redis_client.setex(cache_key, 300, json.dumps(events, default=str))
 
-    events: list[EventOwner] = [
-        EventOwner(**event) for event in events if event["owner"]["_id"] == user_id
-    ]
     if not events:
         raise HTTPException(status_code=404, detail="No events found for this user")
 
-    return events
+    return [EventOwner(**event) for event in events]
