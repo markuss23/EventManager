afa3b61b2531d46c391dc7b459c3630b60b40797
marek.tremel <tremelmarek@gmail.com>
2025-01-04 12:48:33 +0100

    docker is running for dev

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+  },
   plugins: [react()],
-})
+});
